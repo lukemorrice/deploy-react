c8bb926126b3b6d8cf58643e69b8a1e67756ce61
Luke Morrice <lukemorrice14@gmail.com>
2020-06-22 19:38:24 +0100

    Updates

---
+++ precache-manifest.096e0a090b3405104651cde234a2d945.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30de840de989df966aabde3a71ea8557",
+    "url": "/deploy-react/index.html"
+  },
+  {
+    "revision": "3bd991b06cc376f86a4f",
+    "url": "/deploy-react/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "7e9a597136450b1dd837",
+    "url": "/deploy-react/static/js/2.4cd598cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/deploy-react/static/js/2.4cd598cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3bd991b06cc376f86a4f",
+    "url": "/deploy-react/static/js/main.89b8c92f.chunk.js"
+  },
+  {
+    "revision": "aab61e7f7668ac54ee7a",
+    "url": "/deploy-react/static/js/runtime-main.679a13d3.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/deploy-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
