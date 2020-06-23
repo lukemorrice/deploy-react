57ab493a4b4b6d34fbf01b9db235158a60345426
Luke Morrice <lukemorrice14@gmail.com>
2020-06-23 18:32:51 +0100

    Initial commit. Basic website setup.

--- src/App.js
+++ src/App.js
@@ -1,26 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import { Route, NavLink, HashRouter } from "react-router-dom";
+import Home from "./Home";
+import Stuff from "./Stuff";
+import Contact from "./Contact";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class Main extends Component {
+	render() {
+		return (
+			<HashRouter>
+				<div>
+					<h1 style={{ color: "white", fontSize: 60 }}>Optify</h1>
+					<ul className="header">
+						<li>
+							<NavLink exact to="/">
+								Home
+							</NavLink>
+						</li>
+						<li>
+							<NavLink to="/contact">Contact</NavLink>
+						</li>
+						<li>
+							<NavLink to="/stuff">Support</NavLink>
+						</li>
+					</ul>
+					<div className="content">
+						<Route exact path="/" component={Home} />
+						<Route path="/stuff" component={Stuff} />
+						<Route path="/contact" component={Contact} />
+					</div>
+				</div>
+			</HashRouter>
+		);
+	}
 }
 
-export default App;
+export default Main;
---
+++ src/Stuff.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Stuff extends Component {
+	render() {
+		return (
+			<div>
+				<h2>Support</h2>
+				<p>Support for Optify.</p>
+			</div>
+		);
+	}
+}
+
+export default Stuff;
