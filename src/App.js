5e8baa3c630bfd1e36e7c14becde9e48ab3c0076
Luke Morrice <lukemorrice14@gmail.com>
2020-06-24 13:58:12 +0100

    Added privacy policy

--- src/App.js
+++ src/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from "react";
 import { Route, NavLink, HashRouter } from "react-router-dom";
 import Home from "./Home";
-import Stuff from "./Stuff";
+import Support from "./Support";
 import Contact from "./Contact";
+import PrivacyPolicy from "./PrivacyPolicy";
 import "./index.css";
 
 class Main extends Component {
@@ -12,8 +13,12 @@ class Main extends Component {
 				<div>
 					<h1 style={{ color: "white", fontSize: 60 }}>Optify</h1>
 					<ul className="header">
-						<li>
-							<NavLink exact to="/">
+						<li id="home">
+							<NavLink
+								exact
+								to="/"
+								style={{ borderTopLeftRadius: 18, borderBottomLeftRadius: 18 }}
+							>
 								Home
 							</NavLink>
 						</li>
@@ -21,13 +26,17 @@ class Main extends Component {
 							<NavLink to="/contact">Contact</NavLink>
 						</li>
 						<li>
-							<NavLink to="/stuff">Support</NavLink>
+							<NavLink to="/support">Support</NavLink>
+						</li>
+						<li>
+							<NavLink to="/privacypolicy">Privacy Policy</NavLink>
 						</li>
 					</ul>
 					<div className="content">
 						<Route exact path="/" component={Home} />
-						<Route path="/stuff" component={Stuff} />
+						<Route path="/support" component={Support} />
 						<Route path="/contact" component={Contact} />
+						<Route path="/privacypolicy" component={PrivacyPolicy} />
 					</div>
 				</div>
 			</HashRouter>
